58eec4af71608a072ec1c15341e3b3befa56485d
Nan Li <Nan.Li@bcaa.com>
2019-05-16 16:29:55 -0700

    multipart

--- handler.js
+++ handler.js
@@ -1,148 +1,32 @@
-'use strict';
-
+const { startsWith, trim } = require('ramda');
+const multipart = require('aws-lambda-multipart-parser');
 const fs = require('fs');
-const jwt = require('jsonwebtoken');
 const cd = require('cloudinary');
 const cloudconvert = new (require('cloudconvert'))(process.env.CLOUDCONVERT_APIKEY);
-const { connectDB, get, getIdName, getById, search, getPlayerRating, getPlayerGames, cdList, cdVersion, initdata, backup, updateRating, genrr, gengroup, nogame, getNewGameId, addToList, add, replaceList, replace, update, count } = require('./api');
-const { tap, res, policy } = require('./utils');
+const { tap, res } = require('./utils');
+
+const tmp = '/tmp/1.docx';
 
 cd.config({ cloud_name: 'vttc', api_key: process.env.CLOUDINARY_KEY, api_secret: process.env.CLOUDINARY_SECRET });
 
-module.exports.api = async (event, context, cb) => {
+module.exports.convert = async (event, context, cb) => {
   context.callbackWaitsForEmptyEventLoop = false;
-  // const { doc, id, fields, prop, val, idname, player_rating, rating_date, player_games, lookup } = event.queryStringParameters;
-  // await connectDB();
-  //let r = {};
+  const contentType = event.headers['Content-Type'] || event.headers['content-type'];
 
-  // if (lookup) {
-  //   const version = await cdVersion();
-  //   const cats = await get('cats');
-  //   r = { cats, cdVersion: version };
-  // } else if (idname) {
-  //   r = await getIdName(doc);
-  // } else if (id && doc) {
-  //   r = await getById(doc, id);
-  // } else if (fields || prop) {
-  //   r = await search(doc, prop, val, fields);
-  // } else if (player_rating) {
-  //   r = await getPlayerRating(id, rating_date);
-  // } else if (player_games) {
-  //   r = await getPlayerGames(id);
-  // } else if (doc) {
-  //   r = await get(doc);
-  // }
+  if (!startsWith('multipart/form-data', trim(contentType)))
+    return res('no file');
   
+  const result = multipart.parse(event, true);
+  fs.writeFileSync(tmp, result.f1.content);
   const url = await convert();
   await cd.v2.uploader.upload('http:' + url, { folder: 'docs' });
+
   return res('done');
-  
-  //const f = fs.readFileSync('/tmp/s2.png');
-  // return {
-  //   statusCode: 200,
-  //   body: f.toString('base64'),
-  //   isBase64Encoded: true
-  // };
-
-  // try {
-  //   fs.createReadStream(process.env.LAMBDA_TASK_ROOT + '/s1.docx')
-  //     //.pipe(cloudconvert.convert({ inputformat: 'docx', outputformat: 'png' }))
-  //     //.pipe(fs.createWriteStream('/tmp/s1.docx'))
-  //     .on('finish', function() {
-  //       return res('done');
-  //     })
-  // } catch (e) {
-  //   r = e;
-  //   return res(r);
-  // }
 };
 
-module.exports.admin = async (event, context) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-  const q = event.queryStringParameters;
-  const body = JSON.parse(event.body);
-  const method = event.httpMethod;
-  await connectDB();
-  let r = 'no action';
-
-  if (method === 'COPY') {
-    if (q.initdb) {
-      await initdata();
-      r = await updateRating();
-    } else if (q.bak) {
-      r = await backup();
-    }
-  } else if (method === 'POST') {
-    if (q.cd) {
-      r = await cdList();
-    } else if (q.newgameid) {
-      r = await getNewGameId();
-    } else if (q.genrr) {
-      r = await genrr(body);
-    } else if (q.gengroup) {
-      r = await gengroup(+body.id);
-    } else if (q.nogame) {
-      r = await nogame(body);
-    } else if (q.doc && q.id && q.list) {
-      r = await addToList(q.doc, q.id, q.list, body);
-    } else if (q.doc && q.count) {
-      r = await count(q.doc);
-    } else if (q.doc) {
-      r = await add(q.doc, body);
-    }
-  } else if (method === 'PUT') {
-    if (q.groupmatch) {
-      r = await groupmatch(q.id, q.group, body);
-    } else if (q.doc && q.id && q.list) {
-      r = await replaceList(q.doc, q.id, q.list, body);
-    } else if (q.doc) {
-      r = await replace(q.doc, body);
-    }
-  } else if (method === 'PATCH') {
-    if (q.updaterating) {
-      r = await updaterating();
-    } else if (q.result) {
-      await replaceList('tournaments', q.id, 'games', body);
-      r = await updaterating();
-    } else if (q.doc) {
-      r = await update(q.doc, body);
-    }
-  } else if (method === 'PURGE') {
-    if (q.doc) {
-      r = await drop(q.doc);
-    }
-  }
-
-  return res(r);
-};
-
-module.exports.authorize = async event => {
-    await jwt.verify(event.authorizationToken, process.env.JWT_SECRET);
-    return policy(event.methodArn.replace('random-account-id', '*').replace('random-api-id', '*'));
-};
-
-module.exports.login = async event => {
-  const { username, password } = JSON.parse(event.body);
-
-  if (username != process.env.ADMIN_USER || password != process.env.ADMIN_PASSWORD) {
-    return res({ isAuthenticated: false }, 401);
-  } else {
-    const token = jwt.sign({}, process.env.JWT_SECRET, { expiresIn: '24h' });
-    return res({ isAuthenticated: true, token });
-  };
-};
-
-module.exports.logout = async event => res({ isAuthenticated: false });
-
-
-// https://raw.githubusercontent.com/ln613/n2/master/data/db.json
-
-function sleep(millis) {
-    return new Promise(resolve => setTimeout(resolve, millis));
-}
-
-const convert = async () => new Promise(resolve => //fs.createReadStream(process.env.LAMBDA_TASK_ROOT + '/s2.docx').pipe(
+const convert = async () => new Promise(resolve =>
   cloudconvert.createProcess({ inputformat: 'docx', outputformat: 'png' },
-    (e, c) => c.start({ input: 'download', file: process.env.SRC_DOC, outputformat: 'png' },
-      (e, s) => s.wait((e, o) => resolve(o.data.output.url))))
+    (e, c) => c.start({ input: 'upload', outputformat: 'png' },
+      (e, s) => s.upload(fs.createReadStream(tmp), null,
+        (e, u) => u.wait((e, w) => resolve(w.data.output.url)))))
 );
--- utils.js
+++ utils.js
@@ -1,116 +1,17 @@
 const R = require('ramda');
-const moment = require('moment');
-const util = require('@ln613/util');
 
 const e = {};
 
 e.tap = x => R.tap(console.log, R.isNil(x) ? 'null' : x);
 
-e.serial = (arr, func) => arr.reduce((promise, next) => promise.then(r => func(next).then(r1 => R.append(r1, r))), Promise.resolve([]));
-
-e.sort = R.sort((a, b) => a - b);
-
-e.sortDesc = R.sort((a, b) => b - a);
-
-e.isOdd = n => n % 2 === 1;
-
-const rrCycle = (x, r, l) => x < r ? x - r + l : x - r + 1;
-
-e.rrSchedule = (x, sorted, continuousId) => {
-  const l = sorted ? x : R.sortWith([R.descend(R.prop('rating'))], x);
-  if (e.isOdd(l.length)) l.push({id: null});
-  const t1 = R.range(1, l.length);
-  const t2 = R.range(0, l.length / 2);
-  return t1.map((r, i) => {
-    const l1 = t1.map(n => l[rrCycle(n, r, l.length)]);
-    const l2 = R.insert(0, l[0], l1);
-    return t2
-      .map(n => ({ round: i + 1, home: l2[n].id, away: l2[l.length - n - 1].id }))
-      .filter(t => t.home && t.away)
-      .map((t, j) => ({...t, id: continuousId ? ((i * l.length / 2) + j + 1) : (j + 1) }));
-  })
-}
-
-e.getTeamRating = t => t.players && t.players.length > 1
-  ? R.pipe(R.map(x => +(x.isSub ? 0 : (x.tRating || x.rating))), e.sort, R.takeLast(2), R.sum)(t.players)
-  : 0;
-
-e.rrScheduleTeam = (teams, startDate, ids) => {
-  if (!ids) ids = R.range(1, Math.min(Math.floor(teams.length / 2) + 1, 7));
-
-  return R.compose(
-    rs => rs.map((w, i) => ({ id: i + 1, matches: w.map((m, j) => { m.id = ids[j]; return m; }), date: moment(startDate).add(i, 'week').toDate() })),
-    R.splitEvery(ids.length),
-    R.unnest,
-    e.rrSchedule,
-    R.map(t => ({...t, rating: e.getTeamRating(t)}))
-  )(teams);
-}
-
-e.json2js = x => JSON.parse(x, (k, v) => R.takeLast(4, k).toLowerCase() === 'date' ? new Date(v) : v)
-
-const rdiff = [[3,0],[5,-2],[8,-5],[10,-7],[13,-9],[15,-11],[18,-14],[20,-16],[25,-21],[30,-26],[35,-31],[40,-36],[45,-41],[50,-45],[55,-50]];
-const rdelta = [401,301,201,151,101,51,26,-24,-49,-99,-149,-199,-299,-399];
-
-const rateDiff = (r1, r2) => {
-  const n = rdelta.findIndex(x => x <= r1 - r2);
-  return n === -1 ? R.last(rdiff) : rdiff[n];
-}
-
-e.adjustRating = (g, im = true) => {
-  if (g.isDouble || !g.result || g.result === '0:0') {
-      return g;
-  } else {
-    const p1Win = +g.result[0] > +g.result[2];
-    const d = p1Win ? rateDiff(g.p1Rating, g.p2Rating) : rateDiff(g.p2Rating, g.p1Rating);
-    const c = { p1Diff: p1Win ? d[0] : d[1], p2Diff: p1Win ? d[1] : d[0] };
-    return im ? Object.assign({}, g, c) : Object.assign(g, c);
-  }
-}
-
-e.newRating = (r, d) => Math.max(r + d, 100)
-
-e.sortTeam = R.pipe(R.map(t => [e.getTeamRating(t), t]), R.sortWith([R.descend(R.nth(0))]), R.map(R.nth(1)))
-
-e.numOfGroups = n => Math.pow(2, Math.floor(Math.log10(n / 3) / Math.log10(2)))
-
-e.group = ts => {
-  const n = ts.length;
-  const g = e.numOfGroups(n);
-  return ts.map((t, i) => {
-    const l = Math.floor(i / g);
-    const c = i % g;
-    const group = e.isOdd(l) ? (g - c) : c + 1;
-    return {...t, group};
-  });
-}
-
-e.gengames = (t, t1, t2) => {
-  const team1 = util.findById(t1)(t.teams);
-  const team2 = util.findById(t2)(t.teams);
-  return R.range(0, 5).map(n => ({ id: n + 1, date: t.startDate, t1, t2,
-    p1: +team1.players[n === 1 || n === 4 ? 1 : 0].id,
-    p2: +team2.players[n === 0 || n === 4 ? 1 : 0].id,
-    p3: n === 2 ? team1.players[1].id : undefined,
-    p4: n === 2 ? team2.players[1].id : undefined
-  }));
-}
-
-e.toDateOnly = d => R.is(String, d) ? R.take(10, d) : moment(d).add(8, 'hours').format('YYYY-MM-DD');
-
 e.res = (body, code) => ({
   statusCode: code || 200,
   headers: {
-    'Access-Control-Allow-Origin': '*'
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Origin, Content-Type, Accept',
+    'Access-Control-Allow-Methods': 'POST'
   },
   body: JSON.stringify(body)
 });
-
-e.policy = r => ({
-  principalId: process.env.principalId,
-  policyDocument: { Statement: [ { Action: 'execute-api:Invoke', Effect: 'Allow', Resource: r }, ] }
-});
-
-e.parseCookie = r => R.fromPairs((r.multiValueHeaders.cookie || []).map(c => c.split('=')));
 
 module.exports = e;
